5dd282b57f9a72c98453891ce77a5cfb2049c975
jmweed <weed.jm@gmail.com>
2024-12-30 16:44:37 -0600

    More shit

---
+++ turnresolver.py
@@ -0,0 +1,10 @@
+
+#because all moves are generated using UI's built-in logic this function assumes all moves are valid
+
+
+
+
+def processMoves(gameState, moves):
+    for move in moves:
+    
+    return gameState
